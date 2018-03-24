61329f25620d2d14e5a757ba746dc42e45af7af7
袁彬 <yuanbin404@gmail.com>
2018-03-25 00:08:05 +0800

    添加字段、虚函数声明

--- CompilingPracticum/ast.h
+++ CompilingPracticum/ast.h
@@ -1,72 +1,88 @@
 #pragma once
 #include<string>
 #include<vector>
+#include<utility>
+
+class TypeStruct {
+public:
+	TypeStruct(std::string t){}
+private:
+	std::string simpleType;
+	std::vector<std::pair<int, int>> period;
+};
 
 class ASTNode 
 {
 public:
-	ASTNode():child(std::vector<ASTNode*>()){}
-	virtual std::string codeGenerator();
-	virtual bool scopeCheck();
-	virtual bool typeCheck();
-	virtual std::string getID();
-	virtual std::string getType();
+	ASTNode():child(std::vector<ASTNode&>()){}
+	virtual std::string codeGenerator() = 0;
+	virtual bool scopeCheck() = 0;
+	virtual bool typeCheck() = 0;
+	virtual std::string getID() = 0;
+	virtual TypeStruct getType() = 0;
 protected:
 	std::vector<ASTNode&> child;
-};
-
-class IDNode : public ASTNode 
-{
-public:
-
-private:
-	std::string id;
 };
 
 class VarNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
-
+	std::string id;
+	std::vector<int> subscript;
 };
 
 class ConstNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
-
-};
-
-class PeriodNode : public ASTNode
-{
-public:
-
-private:
-
+	std::string value;
+	std::string simpleType;
 };
 
 class FunctionCallNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
-
+	std::string id;
+	std::string simpleType;
 };
 
 class ExpressionNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
-
+	std::string operation;
 };
 
 class AssignmentNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
 
 };
@@ -74,7 +90,11 @@ private:
 class IfNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
 
 };
@@ -82,7 +102,11 @@ private:
 class ForNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
 
 };
@@ -90,7 +114,11 @@ private:
 class WhileNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
 
 };
@@ -98,7 +126,11 @@ private:
 class CompoundNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
 
 };
@@ -106,47 +138,64 @@ private:
 class VarDeclarationNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
-
+	std::vector<std::string> idlist;
+	TypeStruct type;
 };
 
 class ConstDeclarationNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
-
+	std::string id;
+	std::string value;
 };
 
 class FunctionDeclarationNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
-
-};
-
-class TypeNode : public ASTNode
-{
-public:
-
-private:
-
+	std::string id;
+	std::string simpleType;
 };
 
 class ParameterNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
-
+	bool isVar;
+	std::string simpleType;
+	std::vector<std::string> idlist;
 };
 
 class ProgramNode : public ASTNode
 {
 public:
-
+	std::string codeGenerator() override;
+	bool scopeCheck() override;
+	bool typeCheck() override;
+	std::string getID() override;
+	TypeStruct getType() override;
 private:
 
 };
